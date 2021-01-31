0e89828e8f647da1e074a86d008dc666b9e07500
Louie <louie.bergendal@gmail.com>
2021-01-31 20:10:59 +0100

    logos

--- e-wallet/src/components/CardForm.vue
+++ e-wallet/src/components/CardForm.vue
@@ -4,31 +4,33 @@
     
     <!-- Inputformulär -->
     <form v-on:submit.prevent="addCard">
-
-        <h4>CARD NUMBER</h4> <!-- Allt som skrivs i fältet laddas in i "InputNumber" -->
-        <input type="text" class="number-input" placeholder="xxxx xxxx xxxx xxxx"
-        v-model="inputNumber" required pattern="^[0-9]*$"> 
         <!-- 
             "required" ser till så att fältet måste vara ifyllt innan commit.
             "pattern" ser till att det bara kan skrivas siffror. 
+            "v-model" ser till att allt som skrivs i fältet laddas in i ett variabelnamn.
         -->
 
-        <h4>CARD HOLDER</h4> <!-- Skall vara required.  -->
+        <h4>CARD NUMBER</h4>
+        <input type="text" class="number-input" placeholder="xxxx xxxx xxxx xxxx"
+        v-model="inputNumber" required pattern="^[0-9]*$"> 
+
+
+        <h4>CARD HOLDER</h4>
         <input type="text" class="holder-input" placeholder="xxxx xxxx xxxx xxxx"
-        v-model="inputHolder" required> <!-- Allt som skrivs i fältet laddas in i "InputHolder" -->
+        v-model="inputHolder" required>
 
-        <span>CCV</span> <!-- Bara siffror skrivs in, max 3 tecken och skall vara required.  -->
+        <span>CCV</span>
         <input type="text" class="ccv-input" placeholder="xxxx xxxx xxxx xxxx"
-        v-model="inputCCV" required pattern="^[0-9]*$"> <!-- Allt som skrivs i fältet laddas in i "InputCCV" -->
+        v-model="inputCCV" required pattern="^[0-9]*$">
 
-        <span>VENDOR</span> <!-- Bara siffror skrivs in, max 3 tecken och skall vara required.  -->
+        <span>VENDOR</span>
         <select v-model="inputVendor" class="vendor-input" required>
             <option disabled value="">Please select one</option>
             <option>Bitcoin</option>
             <option>Ninja Bank</option>
             <option>Blockchain inc</option>
             <option>Evil Corp</option>
-        </select> <!-- Det som väljs från dropdown-menyn laddas in i "InputVendor" -->
+        </select>
 
         <button>SUBMIT!!</button> <!-- Knappen autofunkar eftersom att den ligger i en form-tag -->
 
--- e-wallet/src/components/CardItem.vue
+++ e-wallet/src/components/CardItem.vue
@@ -5,7 +5,14 @@
         {{cardItemData.number}}
         {{cardItemData.ccv}}
         {{cardItemData.vendor}}
+        
+        <img v-if="cardItemData.vendorClass == 'bitcoin'" alt="Bitcoin logo" src='../assets/vendor-bitcoin.svg' />
+        <img v-if="cardItemData.vendorClass == 'ninja-bank'" alt="Ninja Bank logo" src='../assets/vendor-ninja-bank.svg' />
+        <img v-if="cardItemData.vendorClass == 'block-chain'" alt="Blockchain inc logo" src='../assets/vendor-block-chain.svg' />
+        <img v-if="cardItemData.vendorClass == 'evil-corp'" alt="Evil Corp logo" src='../assets/vendor-evil-corp.svg' />
     </div>
+
+
 </template>
 
 <script>
@@ -19,17 +26,19 @@ export default {
 
 <style scoped>
     .card {
+        height: 150px;
+        width: 250px;
+
         padding: 10px;
         margin: 10px;
-        width: 300px;
-        border-radius: 25px; 
+        border-radius: 25px;
     }
 
     .card.-evil-corp {
         background-color: red;
     }
     .card.-bitcoin {
-        background-color: yellow;
+        background-color: rgb(255, 238, 0);
     }
     .card.-ninja-bank {
         background-color: black;
